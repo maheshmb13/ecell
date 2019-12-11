3e7e3b51beb691d7270a63f206f25067e6895ea8
Subhang23 <vsubhang4@gmail.com>
2019-12-11 20:13:32 +0530

    changes for registration of ca

--- signin/signup_ca.php
+++ signin/signup_ca.php
@@ -9,7 +9,7 @@
     if(isset($_POST['subData'])){
 		require('../config.php');
         if($_POST['signin']){
-            require('../form.php');
+            require('../form_amb.php');
         }
         else
         require('../login.php');       
@@ -23,16 +23,34 @@
 	<div class="login-html">
 		<form action="" method="post" > 
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked name="signin"><label for="tab-1" class="tab">Sign In</label>
-		<!-- <input id="tab-2" type="radio" name="tab" class="sign-up" name="signup"><label for="tab-2" class="tab">Sign Up</label> -->
+		<input id="tab-2" type="radio" name="tab" class="sign-up" name="signup"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
+			<div class="sign-in-htm">
+				<div class="group">
+					<label for="user" class="label">E-Mail</label>
+					<input id="user" type="email" class="input" required name="Email">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Password</label>
+					<input id="pass" type="password" class="input" data-type="password" required name= "Code">
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Sign In" name="subData">
+				</div>
+				<div class="hr"></div>
+			</div>
 			<div class="sign-up-htm">
 				<div class="group">
 					<label for="user" class="label" name="Name">Name</label>
 					<input id="user" type="text" class="input" required>
 				</div>
 				<div class="group">
-					<label for="pass" class="label">Referral Code</label>
-					<input id="pass" type="password" class="input" data-type="password" required name="Code">
+					<label for="pass" class="label">Password</label>
+					<input id="pass" type="password" class="input" data-type="password" required name="Password">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Confirm Password</label>
+					<input id="pass" type="password" class="input" data-type="password" required name="CPassword">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Email Address</label>
@@ -47,7 +65,7 @@
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
-                    <label for="tab-1"><a href="./signup_ca.php">Register as CA?</a></label>
+                    <label for="tab-1">Register as CA</label>
                 </div>
 			</div>
 		</div>
