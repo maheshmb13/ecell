872f6d26a7b7c839470ded2065c86716e2cf1db5
Subhang23 <vsubhang4@gmail.com>
2019-12-11 19:16:01 +0530

    fixes to normal registration

--- signin/signup.php
+++ signin/signup.php
@@ -3,15 +3,27 @@
 <head>
     
     <link rel="stylesheet" href="y.css">
-    </head>    
+</head> 
+<?php
+
+    if(isset($_POST['subData'])){
+		//require('../config.php');
+        if($_POST['signin']){
+            require('../form.php');
+        }
+        else
+        require('../login.php');       
+    }
+    
+?>   
     
     
 <body>
 <div class="login-wrap">
 	<div class="login-html">
 		<form action="" method="post" > 
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked name="signin"><label for="tab-1" class="tab">Sign In</label>
+		<input id="tab-2" type="radio" name="tab" class="sign-up" name="signup"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
 			<div class="sign-in-htm">
 				<div class="group">
@@ -23,16 +35,13 @@
 					<input id="pass" type="password" class="input" data-type="password">
 				</div>
 				<div class="group">
-					<input type="submit" class="button" value="Sign In">
+					<input type="submit" class="button" value="Sign In" name="subData">
 				</div>
 				<div class="hr"></div>
-				<div class="foot-lnk">
-					<a href="#forgot">Forgot Password?</a>
-				</div>
 			</div>
 			<div class="sign-up-htm">
 				<div class="group">
-					<label for="user" class="label">Username</label>
+					<label for="user" class="label">Name</label>
 					<input id="user" type="text" class="input">
 				</div>
 				<div class="group">
@@ -40,7 +49,7 @@
 					<input id="pass" type="password" class="input" data-type="password">
 				</div>
 				<div class="group">
-					<label for="pass" class="label">Repeat Password</label>
+					<label for="pass" class="label">Confirm Password</label>
 					<input id="pass" type="password" class="input" data-type="password">
 				</div>
 				<div class="group">
@@ -48,7 +57,11 @@
 					<input id="pass" type="text" class="input">
 				</div>
 				<div class="group">
-					<input type="submit" class="button" value="Sign Up">
+					<label for="pass" class="label">College</label>
+					<input id="pass" type="text" class="input">
+				</div>				
+				<div class="group">
+					<input type="submit" class="button" value="Sign Up" name="subData">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
