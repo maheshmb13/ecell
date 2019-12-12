e0e8c547fc45b274ac060d5e733da0d0f66ca90b
Subhang23 <vsubhang4@gmail.com>
2019-12-12 22:30:22 +0530

    minor fixes

--- signin/signup.php
+++ signin/signup.php
@@ -22,8 +22,8 @@
 <div class="login-wrap">
 	<div class="login-html">
 		<form action="" method="post" > 
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked name="signin"><label for="tab-1" class="tab">Sign In</label>
-		<!-- <input id="tab-2" type="radio" name="tab" class="sign-up" name="signup"><label for="tab-2" class="tab">Sign Up</label> -->
+		<!-- <input id="tab-1" type="radio" name="tab" class="sign-in" name="signin"><label for="tab-1" class="tab">Sign In</label> -->
+		<input id="tab-2" type="radio" name="tab" class="sign-up" checked name="signup"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
 			<div class="sign-up-htm">
 				<div class="group">
